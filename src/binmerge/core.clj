0b40cb23c297ed48de556904d2722ea5db763edf
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-04-30 19:57:15 +0530

    read obj

--- src/binmerge/core.clj
+++ src/binmerge/core.clj
@@ -1,6 +1,19 @@
 (ns binmerge.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require '[clojure.java.io :refer [file output-stream input-stream reader]])
+
+(def in (input-stream (file "/Users/subhash/Downloads/table1")))
+
+(defn byte-seq [in]
+  (take-while (partial <= 0) (repeatedly #(.read in))))
+
+(defn bytes->int [b]
+  (-> (map byte b) (byte-array) (biginteger)))
+
+(defn read-obj [s]
+  (let [x (->> s (take 8)(bytes->int))
+        name (->> (drop 8 s) (take x) (map char) (apply str))
+        n (->> (drop (+ 8 x) s) (take 8) (bytes->int))]
+    [name n]))
+
+(-> (read-obj (byte-seq in)) )
