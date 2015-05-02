fe690ac264a3b1f1ce13734441c2d329d064d95c
Subhash Gopalakrishnan <subhashgo@gmail.com>
2015-05-02 11:04:55 +0530

    with writing to out

--- src/binmerge/core.clj
+++ src/binmerge/core.clj
@@ -19,7 +19,6 @@
   (->> (map char b) (apply str)))
 
 
-
 (defn pull-name [s]
   (let [[xseq xrest] (split-at 8 s)
         x (bytes->int xseq)
@@ -38,17 +37,6 @@
     [(assoc m n v) (concat acc nseq vseq) vrest]))
 
 
-(defn merge-attrs [a an b bn acc]
-  (cond
-   (<= an 0)
-   (if (<= bn 0) nil
-     (let [[n nseq nrest] (pull-name b)
-           [v vseq vrest] (pull-name nrest)]
-       (recur a an vrest (dec bn) (concat acc nseq vseq))))
-   (<= bn 0) (recur b bn a an acc)
-   ))
-
-
 (defn pull-obj [s]
   (if-let [s (seq s)]
     (let [[n nseq nrest] (pull-name s)
@@ -57,51 +45,26 @@
       [s n (concat nseq aseq attr-seq) attr-rest])))
 
 
-(defn merger [seqs]
-  (let [mgr (fn []
-              (when-let [[f & r] (->> (map pull-obj seqs) (filter identity) (sort-by second) seq)]
-                (do (println (second f)) (merger (conj (map first r) (last f))))))]
-    mgr))
+(defn merger [seqs out]
+  (fn []
+    (when-let [[f & r] (->> (map pull-obj seqs)
+                            (filter identity)
+                            (sort-by second)
+                            seq)]
+      (do
+        (println (second f))
+        (.write out (byte-array (nth f 2)))
+        (merger (conj (map first r) (last f)) out)))))
 
 
 (def files ["/Users/subhash/Downloads/table1" "/Users/subhash/Downloads/table2" "/Users/subhash/Downloads/table3"])
 
-(let [mgr (merger (map (comp byte-seq input-stream file) (reverse files)))]
-  (trampoline mgr))
+(type (-> (byte-seq in) (byte-array)))
+
+(with-open [out (java.io.FileOutputStream. "/tmp/see")]
+  (let [mgr (merger (map (comp byte-seq input-stream file) (reverse files)) out)]
+    (trampoline mgr)))
 
 
- (-> ((comp byte-seq input-stream file) (first files)) pull-obj last pull-obj last pull-obj last pull-obj)
-
-(defn merge-seq [a b acc]
-  (let [[an anseq aa aaseq aattr aattr-seq arest] (pull-obj a)
-        [bn bnseq ba baseq battr battr-seq brest] (pull-obj a)
-        c (compare an bn)]
-    (cond
-     (< c 0)
-     (recur arest b (concat acc anseq aaseq aattr-seq))
-     (> c 0)
-     (recur a brest (concat acc bnseq baseq battr-seq))
-     :else
-     1)))
-
-
-(->> [[1 2 3] [4 5 6] [7 9]] (vector :begin))
-
-
-
-(defn read-obj [s objs]
-  (let [x (->> s (take 8) bytes->int)
-        name (->> (drop 8 s) (take x) bytes->string)
-        n (->> (drop (+ 8 x) s) (take 8) bytes->int)
-        [rs attrs] (reduce (fn [[s a] _] (read-attr s a)) [(drop (+ 8 x 8) s) {}] (range 0 n))
-        ]
-    [rs (assoc objs name attrs)]))
-
-(defn read-attr [s attrs]
-  (let [y (->> s (take 8) bytes->int)
-        k (->> (drop 8 s) (take y) bytes->string)
-        z (->> (drop (+ 8 y) s) (take 8) bytes->int)
-        v (->> (drop (+ 8 y 8) s) (take z) bytes->string)]
-    [(drop (+ 8 y 8 z) s) (assoc attrs k v)]))
 
 
